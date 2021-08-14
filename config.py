85c70ff99f04063a478b75e11fa56749da9416b6
Metin Yalçınkaya <metin.ylnky00@gmail.com>
2021-08-14 16:02:29 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from os import path
+
+basedir = path.abspath(path.dirname(__file__))
+
+
+class Config:
+
+    # Yandex-Geocoder Api Key
+
+    BEST_API_KEY = 'YOUR_API_KEY'
